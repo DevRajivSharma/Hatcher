04edd33e0d9ca8293fe4ab1d0fec6091b281a3dd
Rajiv Sharma <sharma9769387961@gmail.com>
2025-01-16 20:12:36 +0530

    bug in city suggestion div in dashboar home

--- hatcher_source/citydata/views.py
+++ hatcher_source/citydata/views.py
@@ -4,5 +4,5 @@ from citydata.models import CityState
 
 def search_city(request):
     query = request.GET.get('term', '')  # Get the search term from the request
-    cities = CityState.objects.filter(city__istartswith=query).values_list('city', flat=True)[:5]  # Limit to 10 results
+    cities = CityState.objects.filter(city__istartswith=query).values_list('city','state')[:9]  # Limit to 10 results
     return JsonResponse(list(cities), safe=False)  # Return the data as JSON
